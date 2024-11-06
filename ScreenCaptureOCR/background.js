df7504e96467bf92e5a9bd5c45337691282c1ce6
thgmldl0115 <thgmldl0115@gmail.com>
2024-11-06 17:54:32 +0900

    Add files via upload
    create SCT

---
+++ ScreenCaptureOCR/background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(()=>{
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
+        if(changeInfo.status === "complete" && tab.url){
+            chrome.scripting.executeScript({
+                target : {tabId : tabId},
+                files : ["content.js"]
+            })
+            .then(()=> console.log("content script successfully"))
+            .catch((error)=> console.log("faile", error));
+        }
+    });
+});
---
+++ ScreenCaptureTranslation/background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if(req.action === "captureVisibleTab"){
+        chrome.tabs.captureVisibleTab((imageUri)=>{
+            res({imageUri});
+        });
+        return true;
+    }
+});
